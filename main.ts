67f247b065785be6c39aec55005daa959fc48dc0
TFU-Business <59180944+TFU-Business@users.noreply.github.com>
2021-11-28 18:18:25 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(3)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(4)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+radio.setGroup(1)
+radio.sendNumber(0)
